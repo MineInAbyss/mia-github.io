c036a5ca753b8e218174960136e75e25ae886788
Naatchi <joshpahwa2@gmail.com>
2022-04-11 23:07:24 -0400

    added Scyu's pure css parallax

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,23 +1,32 @@
 import {useEffect, useState} from "react";
-import Parallax from "../components/parallax/Parallax";
 
 export default function Home() {
+
+    var i = 0
+    var x = []
+
+    while(i <= 8) {
+        x.push(
+            <div className={"parallax__layer parallax__layer__" + i}>
+                <img src={"https://firewatchgame.com/images/parallax/parallax" + i++ + ".png"} />
+            </div>
+        )
+    }
+
     return (
         <>
-            <div id={"wrapper"}>
-                <section id={"Parallax"} style={{backgroundColor: '#FFAF1B', height: 1030}}>
-                    <Parallax/>
-                </section>
-                <section id={"content"} style={{backgroundColor: '#210002', position: 'relative'}}>
-                    <div id={"text"} className="h-screen px-6 text-white text-md">
-                        hello! this is a test site utilizing next.js and react for testing parallax.<br/>
-                        all art belongs to <a href={"https://www.camposanto.com"} className={"text-blue-600 text:md hover:underline"}> campo stanto </a> and the <a href={"https://www.firewatchgame.com"} className={"text-blue-600 text:md hover:underline"}> firewatch team</a><br/>
+            <div className={"wrapper"}>
+                {x}
+                <div className={"site"}>
+                    <div id={"text"} className={"p-5"}>
+                        hello this is a site for testing parallax now in: <img src="https://cdn.discordapp.com/attachments/838991750584074240/963271680845836368/cooltext408621478289695.gif"/><br/>
+                        all artwork still belongs to <a href={"https://www.camposanto.com"} className={"text-blue-600 hover:underline"}> campo santo </a> and the <a href={"https://www.firewatchgame.com"} className={"text-blue-600 hover:underline"}> firewatch team </a> <br/>
                         <br/>
-                        if you want to check out the repository for this website <a href={"https://github.com/Naatchi/miaGithub.io"} className={"text-blue-600 text:md hover:underline"}> click here! </a><br/>
+                        if you would like to check out this now OFFICIAL!!! repo <a href={"https://github.com/MineInAbyss/mia-github.io"} className={"text-blue-600 hover:underline"}> click here! </a>
                         <br/>
-                        reverse engineered with love 💕
+                        made with love ❤️
                     </div>
-                </section>
+                </div>
             </div>
         </>
     )
