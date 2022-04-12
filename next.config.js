eb641455bdb58ebf557f4a959bfbace61f3884b7
Naatchi <joshpahwa2@gmail.com>
2022-04-11 23:37:20 -0400

    test commit

--- next.config.js
+++
@@ -1,7 +0,0 @@
-const isProd = process.env.NODE_ENV === 'production'
-
-/*module.exports = {
-    assetPrefix: isProd ? '/mia-github.io' : '',
-    basePath: process.env.NEXT_PUBLIC_BASE_PATH,
-    assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
-} */
