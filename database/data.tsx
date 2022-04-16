255d173a64549eac4a749b65247725bc51139e35
Naatchi <joshpahwa2@gmail.com>
2022-04-16 18:31:38 -0400

    fixed internal site pathing

--- database/data.tsx
+++ database/data.tsx
@@ -1,13 +1,13 @@
 export const data = {
     links: {
-        downloads: '/downloads',
-        faq: '/faq',
-        github: '/github',
-        community: '/community',
-        contribute: '/contribute',
-        developers: '/developers',
-        builders: '/builders',
-        artists: '/artists',
+        downloads: '/mia-github.io/downloads',
+        faq: '/mia-github.io/faq',
+        github: '/mia-github.io/github',
+        community: '/mia-github.io/community',
+        contribute: '/mia-github.io/contribute',
+        developers: '/mia-github.io/developers',
+        builders: '/mia-github.io/builders',
+        artists: '/mia-github.io/artists',
     },
     images: {
         devButton: 'https://cdn.discordapp.com/attachments/934287357207781386/964943433712152596/Untitled.png',
