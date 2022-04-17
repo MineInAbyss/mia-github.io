709faf4134d7dea08645716691d749ab5ec2b88f
Naatchi <joshpahwa2@gmail.com>
2022-04-16 21:38:03 -0400

    fixed team card mobile spacing

--- src/pages/developers.tsx
+++ src/pages/developers.tsx
@@ -20,23 +20,30 @@ export default function developers() {
                         <div className={"flex justify-center text-gray-400 text-2xl font-bold py-12"}>
                             Developers
                         </div>
-                        <div className={"flex space-x-4 justify-center"}>
-                            <TeamMemberCard />
-                            <TeamMemberCard />
-                            <TeamMemberCard />
-                            <TeamMemberCard />
+                        <div className={"grid grid-cols-1 place-items-center gap-4 md:flex md:justify-center"}>
+                            <div className={"flex space-x-4"}>
+                                <TeamMemberCard />
+                                <TeamMemberCard />
+                            </div>
+                            <div className={"flex space-x-4"}>
+                                <TeamMemberCard />
+                                <TeamMemberCard />
+                            </div>
                         </div>
                     </div>
                     <div>
                         <div className={"flex justify-center text-gray-400 text-2xl font-bold py-12"}>
                             Junior Developers
                         </div>
-                        <div className={"flex space-x-4 justify-center"}>
-                            <TeamMemberCard />
-                            <TeamMemberCard />
-                            <TeamMemberCard />
-                            <TeamMemberCard />
-                            <TeamMemberCard />
+                        <div className={"grid grid-cols-1 place-items-center gap-4 md:flex md:justify-center"}>
+                            <div className={"flex space-x-4"}>
+                                <TeamMemberCard />
+                                <TeamMemberCard />
+                            </div>
+                            <div className={"flex space-x-4"}>
+                                <TeamMemberCard />
+                                <TeamMemberCard />
+                            </div>
                         </div>
                     </div>
                 </>
